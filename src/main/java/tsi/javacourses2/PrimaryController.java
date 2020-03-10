6f689d80f19b76f30f9c6123b66ae55c9f500d21
JavaCourses1 <atdk.fb@gmail.com>
2020-03-10 18:52:07 +0200

    to Random number method insert

--- src/main/java/tsi/javacourses2/PrimaryController.java
+++ src/main/java/tsi/javacourses2/PrimaryController.java
@@ -1,6 +1,7 @@
 package tsi.javacourses2;
 
 import java.io.IOException;
+import java.util.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,7 +9,7 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
 
 public class PrimaryController {
-
+    private static List<Integer> myNumbers;
     private int count;
     public Spinner<Integer> num1;
     public Spinner<Integer> num2;
@@ -16,7 +17,23 @@ public class PrimaryController {
     public Spinner<Integer> num4;
     public TableView<Turn> turnsTable;
 
-    public void doTurn() {
+
+
+    public static void  initialize() { //это готовый метод для загаданного 4х значного рандомного числа.
+        Set<Integer> tmp = new LinkedHashSet<>();
+        Random rand = new Random();
+        while (tmp.size() < 4) {
+            int i = rand.nextInt(10); // rand: 3 4 3 3 4 4 4 3 3 3 ... 9 ... 7. Тоесть Set проверяет на уникальность значения
+          //   set: 3 4 9 7
+            tmp.add(i);
+        }
+         myNumbers = List.copyOf(tmp);
+        System.out.println(myNumbers);
+    }
+
+
+
+    public void doTurn() { //этот метод возникает при нажатии на кнопку Go
         count++;
         int n1 = num1.getValue();
         int n2 = num2.getValue();
